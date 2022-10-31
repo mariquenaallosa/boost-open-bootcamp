84d82f3e26f6b3d8a7393a9cf406d126b12cc2a6
mariquenaallosa <allosamariquena@gmail.com>
2022-10-31 01:06:14 -0300

    update loop

---
+++ src/com/company/SwitchCase.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class SwitchCase {
+
+    public static void main(String[] args) {
+        String weather = "sunny";
+
+        switch (weather){
+            case "sunny":
+                System.out.println("El tiempo es soleado");
+                break;
+            case  "cloudy":
+                System.out.println("el tiepo está nublado");
+                break;
+            default:
+                System.out.println("No se ha podido identificar el clima");
+                break;
+        }
+
+
+
+    }
+}
---
+++ src/com/company/WhileLoop.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+     //   boolean check = true;
+
+
+        int count = 0;
+        while (count < 10){
+            count++;
+            if(count == 6)
+                break; //rompe
+            // continue; -> saltea
+
+        System.out.println("Hola mundo " + count);
+
+        }
+        System.out.println("fin");
+
+    }
+}

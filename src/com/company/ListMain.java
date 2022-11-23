874758e9e78459f80fd894ee841a9844c2cdd35f
mariquenaallosa <allosamariquena@gmail.com>
2022-11-23 00:24:31 -0300

    Listas
    Creación de método main y explicación de la clase “List”
    Creación de un “ArrayList”
    Método “add”
    Iterar con un bucle “forEach”
    Depuración de código
    Uso de ArrayLists
    Método “toString”

---
+++ src/com/company/ListMain.java
@@ -0,0 +1,43 @@
+package com.company;
+import java.util.ArrayList;
+import java.util.List;
+public class ListMain {
+
+
+    public static void main(String[] args) {
+        // Son como arrays pero dinámicos
+        // Es una interface
+        List<String> nombres = new ArrayList<>();
+
+        nombres.add("nombre1");
+        nombres.add("nombre2");
+        nombres.add("nombre3");
+        nombres.add("nombre4");
+        nombres.add("nombre5");
+        nombres.add("nombre6");
+        nombres.add("nombre7");
+        nombres.add("nombre8");
+        nombres.add("nombre9");
+
+        System.out.println(nombres);
+
+       for (String nombre : nombres){
+           System.out.println(nombre);
+        }
+        List<Coche> coches = new ArrayList<>();
+
+       coches.add(new CocheElectrico());
+        coches.add(new CocheElectrico());
+        coches.add(new CocheElectrico());
+        coches.add(new CocheElectrico());
+        coches.add(new CocheElectrico());
+
+        System.out.println(coches);
+
+        for (Coche coche: coches
+             ) {
+            System.out.println(coche);
+        }
+
+    }
+}

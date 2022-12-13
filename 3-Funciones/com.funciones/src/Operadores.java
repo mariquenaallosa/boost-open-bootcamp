bc459bc356678136fd8cc807c8791d76a01d983d
mariquenaallosa <allosamariquena@gmail.com>
2022-12-13 20:23:45 -0300

    Operadores

---
+++ 3-Funciones/com.funciones/src/Operadores.java
@@ -0,0 +1,23 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //aritmeticos
+        // + - / * %
+        int numero1= 10;
+        int numero2= 20;
+
+        int resultadoSuma = numero1 + numero2;
+
+        System.out.println(resultadoSuma);
+
+        int resultadoResta = numero1 - numero2;
+
+        System.out.println(resultadoResta);
+
+
+        // comparacion
+        // == != > >= < <=
+
+        
+
+    }
+}

76a8cc4672639ae150b93f3b5a333cae9e76240f
Mariquena <allosamariquena@gmail.com>
2022-12-18 18:03:08 -0300

    Create EmpleadoCRUD.java

---
+++ 6-Interfaces/com.interface/src/main/java/org/interfaces/EmpleadoCRUD.java
@@ -0,0 +1,16 @@
+package org.interfaces;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class EmpleadoCRUD {
+
+    List<Empleado> empleados = new ArrayList<Empleado>();
+    //guardar un empleado
+    public void guardar(Empleado empleado){
+
+    }
+}

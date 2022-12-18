fc24cbedb09c930b996e2126e7558ec48be8fd78
Mariquena <allosamariquena@gmail.com>
2022-12-18 18:03:14 -0300

    Create Main.java

---
+++ 6-Interfaces/com.interface/src/main/java/org/interfaces/Main.java
@@ -0,0 +1,16 @@
+package org.interfaces;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        EmpleadoCRUD empleadoCRUD = new EmpleadoCRUD();
+
+
+        Empleado juanito = new Empleado("nombre",34,25920,true);
+
+
+
+    }
+}

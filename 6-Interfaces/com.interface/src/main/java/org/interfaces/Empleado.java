b74c261623ede0084f3319d97d0f58209a7153e5
Mariquena <allosamariquena@gmail.com>
2022-12-18 18:03:03 -0300

    Create Empleado.java

---
+++ 6-Interfaces/com.interface/src/main/java/org/interfaces/Empleado.java
@@ -0,0 +1,28 @@
+package org.interfaces;
+
+public class Empleado {
+
+
+    //1. atributos
+    String nombre;
+    int edad;
+    double salario;
+    boolean alta;
+
+    //2. constructores
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, int edad, double salario, boolean alta) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.salario = salario;
+        this.alta = alta;
+    }
+
+    //3. métodos
+
+
+
+}

07b1d6757a8f19e0baaca347c5d9e28a36e71bf4
Mariquena Allosa <allosamariquena@gmail.com>
2022-12-18 20:11:35 -0300

    update

--- 6-Interfaces/com.interface/src/main/java/org/interfaces/EmpleadoCRUD.java
+++ 6-Interfaces/com.interface/src/main/java/org/interfaces/EmpleadoCRUD.java
@@ -4,13 +4,39 @@ package org.interfaces;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/*
+* C -> CREATE
+* R -> READ/RETRIEVE
+* U -> UPDATE
+* D -> DELETE
+ */
 
 public class EmpleadoCRUD {
 
     List<Empleado> empleados = new ArrayList<Empleado>();
+
+    /*
+    *
+    * Una interface seria como un contrato que define una serie de metodos de que hay que hacer pero no los hace sino que hay una clase que implementa la interfaz y es la que realiza los metodos
+    *
+    * Un ejemplo de Interface seria por ejemlo -> List
+    *
+    *
+     */
+
+    //CREATE
     //guardar un empleado
+    //actuaria como una base de datos
+
+
     public void guardar(Empleado empleado){
 
+        empleados.add(empleado);
+
+    }
+
+    // RECUPERAR EMPLEADOS
+    public findAll(){
+        
     }
 }
--- 6-Interfaces/com.interface/src/main/java/org/interfaces/Main.java
+++ 6-Interfaces/com.interface/src/main/java/org/interfaces/Main.java
@@ -8,9 +8,14 @@ public class Main {
         EmpleadoCRUD empleadoCRUD = new EmpleadoCRUD();
 
 
-        Empleado juanito = new Empleado("nombre",34,25920,true);
+        Empleado juanito = new Empleado("Juanito",30,25920,true);
+        Empleado patricia = new Empleado("Patricia",27,25920,false);
+        Empleado ruben = new Empleado("Ruben",40,40000s,true);
 
-
+        // GUARDAR EMPLEAOS
+        empleadoCRUD.guardar(juanito);
+        empleadoCRUD.guardar(patricia);
+        empleadoCRUD.guardar(ruben);
 
     }
 }

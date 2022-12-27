d38cbf845b400ada8918d2d6174a03c862757d38
Mariquena <allosamariquena@gmail.com>
2022-12-27 20:04:48 -0300

    consolidated conditional

--- ob-refactoring/src/main/java/consolidatedconditional/good/Main.java
+++ ob-refactoring/src/main/java/consolidatedconditional/good/Main.java
@@ -1,5 +1,7 @@
 package consolidatedconditional.good;
 
+import static java.awt.DefaultKeyboardFocusManager.sendMessage;
+
 public class Main {
     private double extraSalary;
     private int seniority;
@@ -11,22 +13,28 @@ public class Main {
 
     double calculateExtraSalary() {
         double result = 0;
-        if (seniority<1){
-            return 0;
+        if(isElegibleExtraSalary()){
+            result = 500;
+            sendMessage(); // método que se repite en la condition
+        }else {
+            result = 0;
+            sendMessage(); // método que se repite en la condición
         }
-        if (education<1){
-            return 0;
-        }
-        if (incidents>10){
-            return 0;
-        }
-        if (!certification){
-            return 0;
-        };
-
-        // Calculate extra salary
-        //...
         return result;
+
+    }
+
+    private void sendMessage(){
+        // send email
+        // connect smtp
+        System.out.println("Sending message");
+    }
+
+    private boolean isElegibleExtraSalary(){
+        // agrupar condiciones utilizando AND (&&) y OR(||)
+        boolean estudios = seniority < 1 || education < 1;
+        boolean antiguedad = incidents > 10 && certification;
+        return estudios || antiguedad;
     }
 
 

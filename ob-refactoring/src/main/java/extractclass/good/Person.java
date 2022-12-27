1202c3388bea5fb1e7bfa783bf1eb2595fc206c7
Mariquena <allosamariquena@gmail.com>
2022-12-27 20:15:49 -0300

    extract class

---
+++ ob-refactoring/src/main/java/extractclass/good/Person.java
@@ -0,0 +1,14 @@
+package extractclass.good;
+
+public class Person {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+
+    private Address address;
+
+}
+
+

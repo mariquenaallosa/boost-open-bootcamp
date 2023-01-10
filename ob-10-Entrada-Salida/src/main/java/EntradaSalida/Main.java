cf70a22dd448ea80b2b89fb5fcadd0e2b2327e6d
Mariquena <allosamariquena@gmail.com>
2023-01-10 04:55:24 -0300

    INputStream

---
+++ ob-10-Entrada-Salida/src/main/java/EntradaSalida/Main.java
@@ -0,0 +1,20 @@
+package EntradaSalida;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+/*
+* -->InputStream<--
+* ->nos devuelve un flujo de bytes
+*FileInputStream()-> leer ficheros
+*/
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        InputStream fichero = new FileInputStream();
+
+
+    }
+}
